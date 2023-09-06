3220ef0822f0662216c1d8700d8a4eba5545448b
jzanoum <141528988+jzanoum@users.noreply.github.com>
2023-09-06 23:48:38 +0200

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, jsonify
+import yt_dlp
 
 app = Flask(__name__)
 
+@app.route("/download", methods=["GET"])
+def download():
+    # Get the parameters from the request
+    url = request.args.get("url")
+    format = request.args.get("format")
+    folder = request.args.get("folder")
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    # Create a YoutubeDL object with the desired options
+    ydl_opts = {
+        "format": format,
+        "outtmpl": folder + "/%(title)s.%(ext)s",
+        "quiet": True
+    }
+    ydl = yt_dlp.YoutubeDL(ydl_opts)
+
+    # Try to download the video and return a JSON response
+    try:
+        ydl.download([url])
+        info = ydl.extract_info(url, download=False)
+        filename = ydl.prepare_filename(info)
+        return jsonify({"status": "success", "file": filename})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
 
 
 if __name__ == '__main__':
